4d46dab367d2c192c0858112a30a1ac68ff161c4
zacfer <zacfer@gmail.com>
2017-06-07 18:25:55 +0100

    Test fun initial commit

---
+++ TestFun/Participant.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFun
+{
+    class Participant
+    {
+        public string Name
+        {
+            get;
+        }
+       public int Score
+        {
+            get;
+        }
+        public decimal Prize
+        {
+            get;
+            set;
+        }
+
+        public Participant(string name, int score, decimal prize)
+        {
+            this.Name = name;
+            this.Score = score;
+            this.Prize = prize;
+        }
+
+        public Participant(string name, int score)
+        {
+            this.Name = name;
+            this.Score = score;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Score: {Score}, Prize: {Prize}";
+        }
+    }
+}
---
+++ TestFun/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var participant1 = new Participant("Zac", 1);
+            var participant2 = new Participant("Denis", 2);
+            var participant3 = new Participant("Eoin", 3);
+
+            var participants = new List<Participant>
+            {
+                participant1,
+                participant2,
+                participant3
+            };
+
+            var prizes = new Dictionary<int, decimal>
+            {
+                { 1, 3.45m },
+                { 2, 2.34m },
+                { 3, 1.23m }
+            };
+
+            AwardPrizes(participants, prizes);
+        }
+
+        static void AwardPrizes(IList<Participant> participants, IDictionary<int, decimal> prizes)
+        {
+            var sortedParticipants = participants.OrderByDescending(x => x.Score).ToList();
+
+            int i = 0;
+            sortedParticipants.ForEach(Participant => Participant.Prize = prizes[++i]);
+
+            sortedParticipants.ForEach(Console.WriteLine);
+        }
+    }
+}
